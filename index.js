b948454558a3e90197d67a66ad5f946b106e6d01
garciatestk-cloud <garciatestk@gmail.com>
2026-07-16 15:09:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+Client,
+GatewayIntentBits,
+Partials,
+Collection
+} = require("discord.js");
+
+const config = require("./config");
+const ready = require("./ready");
+const interactionCreate = require("./interactionCreate");
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildMembers,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent,
+GatewayIntentBits.DirectMessages
+],
+
+partials: [
+Partials.Channel,
+Partials.Message,
+Partials.User
+]
 });
 
-client.once("ready", () => {
-    console.log(`✅ Conectado como ${client.user.tag}`);
+client.commands = new Collection();
+
+client.config = config;
+
+client.once("ready", async () => {
+
+console.clear();
+
+console.log("========================================");
+console.log(`🤖 Bot conectado como ${client.user.tag}`);
+console.log(`🆔 ${client.user.id}`);
+console.log("========================================");
+
+await ready(client);
+
+});
+
+client.on("interactionCreate", async interaction => {
+
+await interactionCreate(interaction, client);
+
 });
 
 client.login(process.env.TOKEN);
