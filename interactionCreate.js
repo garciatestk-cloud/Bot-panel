57c9d6beae3677a538f8bf49e0594b66f00adb51
garciatestk-cloud <garciatestk@gmail.com>
2026-07-16 15:16:39 -0600

    Update interactionCreate.js

--- interactionCreate.js
+++ interactionCreate.js
@@ -1,1 +1,84 @@
+const {
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ActionRowBuilder
+} = require("discord.js");
 
+
+module.exports = async (interaction) => {
+
+
+    // Detectar el menú del panel
+    if (
+        interaction.isStringSelectMenu() &&
+        interaction.customId === "shop_panel"
+    ) {
+
+
+        if (interaction.values[0] === "venta") {
+
+
+            const modal = new ModalBuilder()
+                .setCustomId("venta_modal")
+                .setTitle("Venta de Objetos");
+
+
+            const objeto = new TextInputBuilder()
+                .setCustomId("objeto")
+                .setLabel("¿Qué es lo que quieres vender?")
+                .setStyle(TextInputStyle.Paragraph)
+                .setPlaceholder("Escribe el objeto y detalles...")
+                .setRequired(true);
+
+
+            const precio = new TextInputBuilder()
+                .setCustomId("precio")
+                .setLabel("¿Cuál es tu precio?")
+                .setStyle(TextInputStyle.Short)
+                .setPlaceholder("Ejemplo: 15 USD / 500 Robux")
+                .setRequired(true);
+
+
+            const acuerdo = new TextInputBuilder()
+                .setCustomId("acuerdo")
+                .setLabel("¿Estás de acuerdo con lo mencionado?")
+                .setStyle(TextInputStyle.Short)
+                .setPlaceholder("Sí / No")
+                .setRequired(true);
+
+
+            const cuestionar = new TextInputBuilder()
+                .setCustomId("cuestionar")
+                .setLabel("¿Cuestionarás al comprador?")
+                .setStyle(TextInputStyle.Short)
+                .setPlaceholder("Sí / No")
+                .setRequired(true);
+
+
+
+            modal.addComponents(
+
+                new ActionRowBuilder()
+                    .addComponents(objeto),
+
+                new ActionRowBuilder()
+                    .addComponents(precio),
+
+                new ActionRowBuilder()
+                    .addComponents(acuerdo),
+
+                new ActionRowBuilder()
+                    .addComponents(cuestionar)
+
+            );
+
+
+            await interaction.showModal(modal);
+
+        }
+
+    }
+
+
+};
